b83c882f5b19fd0ca92cc1c520a47a63b0ca9fb0
nurseda diker <54947744+nurseda-diker@users.noreply.github.com>
2021-05-02 00:44:06 +0300

    Update InstructorManager.java

--- JavaCampDay3/src/javacampday3/InstructorManager.java
+++ JavaCampDay3/src/javacampday3/InstructorManager.java
@@ -2,10 +2,6 @@
 package javacampday3;
 
 public class InstructorManager {
-    public void addCourse(String courseName){
-        System.out.println("----------");
-        System.out.println("Yeni kursunuz eklendi: "+courseName);
-    }
     public void attributesInstructer(Instructor instructor){
         System.out.println("----------");
         System.out.println("Eğitmenin adı: "+instructor.getFirstName() + "\nSoyadı: "+instructor.getLastName()+"\nId'si: "+instructor.getInstructorId()+"\nMaaşı: "+instructor.getSalary());
